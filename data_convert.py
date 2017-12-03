c9f1e5919ea12c87992a6b2ea7101db5e3ae625c
YanzheL <lee.yanzhe@yanzhe.org>
2017-12-03 08:42:27 +0800

    fix dup

--- data_convert.py
+++ data_convert.py
@@ -5,24 +5,6 @@ from pymysql import *
 
 import proto_files.compiled.Email_pb2 as EmailPb2
 
-conn = connect(
-    host='hk1.lee-service.com',
-    user='mathteam',
-    password='HITmathTEAM',
-    db='enron',
-    charset='utf8mb4',
-    cursorclass=cursors.DictCursor
-)
-
-dataset_size = 255636
-
-# dataset_size = 25
-
-batch_size = 500
-
-
-# batch_size = 10
-
 
 def contains_alpha(s):
     return re.match(r'.*[a-zA-Z0-9]+', s)
@@ -32,47 +14,66 @@ def pack_data(email):
     return (email.ByteSize()).to_bytes(4, byteorder='little', signed=False) + email.SerializeToString()
 
 
-with conn.cursor() as cursor:
-    steps = dataset_size // batch_size + 1
-    with open('data/enron_emails.pb', 'wb') as f:
-        for i in range(steps):
-            if (i * batch_size > dataset_size):
-                break
-            cursor.execute('''
-                SELECT messages.messageid,senderid as `from`, recipientid as `to`,messages.subject as `subject`,bodies.body
-                FROM messages,bodies,recipients
-                WHERE messages.messageid=bodies.messageid AND messages.messageid=recipients.messageid
-                ORDER BY messages.messageid
-                LIMIT %d,%d
-                ''' % (i * batch_size, batch_size))
-            for doc in cursor.fetchall():
-                # print("|".center(200, '-'))
-                if not doc['subject']:
-                    doc['subject'] = 'NONE'
-                # print("SUBJECT %s" % doc['subject'])
-                body = []
-                for line in doc['body'].splitlines():
-                    if line.find('---') != -1:
-                        break
-                    body.append(line)
-                body = ''.join(body)
-                # print(body)
-                sent_tks = [s for s in sent_tokenize(body) if contains_alpha(s)]
+def convert(path):
+    conn = connect(
+        host='hk1.lee-service.com',
+        user='mathteam',
+        password='HITmathTEAM',
+        db='enron',
+        charset='utf8mb4',
+        cursorclass=cursors.DictCursor
+    )
 
-                # print(sent_tks)
-                email = EmailPb2.Email()
-                doc['body'] = body
-                for k in doc.keys():
-                    if k == 'body':
-                        email.body.extend(sent_tks)
-                    else:
-                        setattr(email, k, doc[k])
-                # print(email)
-                # f.writelines()
+    dataset_size = 255636
 
-                d = pack_data(email)
-                # print(bin_size)
-                # print(hex(bin_size))
-                # print(d)
-                f.write(d)
-# print(docs)
+    # dataset_size = 25
+
+    batch_size = 500
+
+    with conn.cursor() as cursor:
+        steps = dataset_size // batch_size + 1
+        with open(path, 'wb') as f:
+            for i in range(steps):
+                if (i * batch_size > dataset_size):
+                    break
+                cursor.execute('''
+                    SELECT messages.messageid,senderid as `from`, recipientid as `to`,messages.subject as `subject`,bodies.body
+                    FROM messages,bodies,recipients
+                    WHERE messages.messageid=bodies.messageid AND messages.messageid=recipients.messageid
+                    ORDER BY messages.messageid
+                    LIMIT %d,%d
+                    ''' % (i * batch_size, batch_size))
+                for doc in cursor.fetchall():
+                    # print("|".center(200, '-'))
+                    if not doc['subject']:
+                        doc['subject'] = 'NONE'
+                    # print("SUBJECT %s" % doc['subject'])
+                    body = []
+                    for line in doc['body'].splitlines():
+                        if line.find('---') != -1:
+                            break
+                        body.append(line)
+                    body = ''.join(body)
+                    # print(body)
+                    sent_tks = [s for s in sent_tokenize(body) if contains_alpha(s)]
+
+                    # print(sent_tks)
+                    email = EmailPb2.Email()
+                    doc['body'] = body
+                    for k in doc.keys():
+                        if k == 'body':
+                            email.body.extend(sent_tks)
+                        else:
+                            setattr(email, k, doc[k])
+                    # print(email)
+                    # f.writelines()
+
+                    d = pack_data(email)
+                    # print(bin_size)
+                    # print(hex(bin_size))
+                    # print(d)
+                    f.write(d)
+
+
+if __name__ == '__main__':
+    convert('data/enron_emails.pb')
--- read_data.py
+++ read_data.py
@@ -2,11 +2,13 @@ import proto_files.compiled.Email_pb2 as EmailPb2
 from data_convert import pack_data
 
 
-def readobj(path):
+def readobj(path, limit):
     ct = 0
     with open(path, 'rb') as f:
         bs = f.read(4)
         while bs:
+            if limit != 0 and ct >= limit:
+                break
             size = int.from_bytes(bs, 'little', signed=False)
             em = EmailPb2.Email()
             em.ParseFromString(f.read(size))
@@ -16,11 +18,16 @@ def readobj(path):
     print(ct)
 
 
-if __name__ == '__main__':
-    with open('data/enron_emails_uniq.pb', 'wb') as f:
+def construct_no_dup(src, dst, limit):
+    with open(dst, 'wb') as f:
         seen = set()
-        for em in readobj('data/enron_emails.pb'):
+        for em in readobj(src, limit):
+            print(em)
             triple = em.messageid, getattr(em, 'from'), em.to
             if triple not in seen:
                 seen.add(triple)
                 f.write(pack_data(em))
+
+
+if __name__ == '__main__':
+    construct_no_dup('data/enron_emails.pb', 'data/enron_emails_uniq.pb', 0)
