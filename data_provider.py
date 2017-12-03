5637ffed5648c5cac9c40164dcc694f9303911c2
YanzheL <lee.yanzhe@yanzhe.org>
2017-12-03 16:52:40 +0800

    by people

---
+++ data_provider.py
@@ -0,0 +1,52 @@
+from read_data import readobj
+from gensim import corpora, models, similarities
+from gensim.models.doc2vec import Doc2Vec
+from nltk.corpus import stopwords
+from collections import defaultdict
+
+
+class DataSet(object):
+    stopwds = set(stopwords.words('english'))
+    _num_examples = 240000
+
+    def __init__(self):
+        self.gener = readobj('data/train.pb', 0)
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    def process_sentence(self, sentences):
+        texts = [
+            [word for word in sentence.lower().split() if word not in self.stopwds]
+            for sentence in sentences
+        ]
+
+        frequency = defaultdict(int)
+        for text in texts:
+            for token in text:
+                frequency[token] += 1
+
+        texts = [
+            [token for token in text if frequency[token] > 1]
+                 for text in texts
+        ]
+        dictionary = corpora.Dictionary(texts)
+        corpus = [dictionary.doc2bow(text) for text in texts]
+
+        tfidf = models.TfidfModel(corpus)
+        corpus_tfidf = tfidf[corpus]
+
+
+    def filter_stopwds(self, s):
+        return ''.join([w for w in s.split().lower() if w not in self.stopwds])
+
+    def next_batch(self, batch_size):
+        ems = []
+        try:
+            for i in range(batch_size):
+                em = self.gener.__next__()
+        except StopIteration:
+            pass
+
+        pass
---
+++ inference.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+
+INPUT_NODE = 200
+
+OUTPUT_NODE = 87474
+
+LAYER_NODE = 10
+
+
+def get_weight_variable(shape, regularizer):
+    weights = tf.get_variable("weights", shape, initializer=tf.truncated_normal_initializer(stddev=0.1))
+    if regularizer != None: tf.add_to_collection('losses', regularizer(weights))
+    return weights
+
+
+def inference_multi(input_tensor, regularizer, num):
+    layer = input_tensor
+    for i in range(num):
+        in_node = INPUT_NODE if i == 1 else LAYER_NODE
+        out_node = OUTPUT_NODE if i == num - 1 else LAYER_NODE
+        with tf.variable_scope('layer%d' % (i + 1)):
+            weights = get_weight_variable(
+                [in_node, out_node],
+                regularizer)
+            biases = tf.get_variable("biases", [out_node], initializer=tf.constant_initializer(0.0))
+            layer = tf.nn.relu(tf.matmul(input_tensor, weights) + biases)
+
+    return layer
+
+
+def inference(input_tensor, regularizer):
+    with tf.variable_scope('layer1'):
+        weights = get_weight_variable([INPUT_NODE, LAYER_NODE], regularizer)
+        biases = tf.get_variable("biases", [LAYER_NODE], initializer=tf.constant_initializer(0.0))
+        layer1 = tf.nn.relu(tf.matmul(input_tensor, weights) + biases)
+
+    with tf.variable_scope('layer2'):
+        weights = get_weight_variable([LAYER_NODE, OUTPUT_NODE], regularizer)
+        biases = tf.get_variable("biases", [OUTPUT_NODE], initializer=tf.constant_initializer(0.0))
+        layer2 = tf.matmul(layer1, weights) + biases
+
+    return layer2
