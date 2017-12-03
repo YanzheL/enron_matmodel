73ab21bfc44ef41414b05308d7a0cee01ab6087d
YanzheL <lee.yanzhe@yanzhe.org>
2017-12-03 08:25:07 +0800

    fix dup

--- read_data.py
+++ read_data.py
@@ -1,12 +1,8 @@
-path = 'data/enron_emails.pb'
-
 import proto_files.compiled.Email_pb2 as EmailPb2
 from data_convert import pack_data
 
 
 def readobj(path):
-    seen = set()
-    ret = []
     ct = 0
     with open(path, 'rb') as f:
         bs = f.read(4)
@@ -14,18 +10,17 @@ def readobj(path):
             size = int.from_bytes(bs, 'little', signed=False)
             em = EmailPb2.Email()
             em.ParseFromString(f.read(size))
-            triple = em.messageid, getattr(em, 'from'), em.to
-            if triple not in seen:
-                seen.add(triple)
-                ret.append(em)
-                ct += 1
+            yield em
+            ct += 1
             bs = f.read(4)
-    with open('data/enron_emails_uniq.pb', 'wb'):
-        for e in ret:
-            f.write(pack_data(e))
     print(ct)
-    return ret
 
 
 if __name__ == '__main__':
-    readobj(path)
+    with open('data/enron_emails_uniq.pb', 'wb') as f:
+        seen = set()
+        for em in readobj('data/enron_emails.pb'):
+            triple = em.messageid, getattr(em, 'from'), em.to
+            if triple not in seen:
+                seen.add(triple)
+                f.write(pack_data(em))
